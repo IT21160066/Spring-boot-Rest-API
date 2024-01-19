8bd2625dd8b71c2de551e4d929ecece3fcf2572a
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2024-01-20 01:34:00 +0530

    mysql, unit testing

---
+++ src/test/java/com/sb/Springboottute/RestAPI/service/DepartmentServiceTest.java
@@ -0,0 +1,45 @@
+package com.sb.Springboottute.RestAPI.service;
+
+import com.sb.Springboottute.RestAPI.entity.Department;
+import com.sb.Springboottute.RestAPI.repository.DepartmentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DepartmentServiceTest {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @MockBean
+    private DepartmentRepository departmentRepository;
+
+    @BeforeEach
+    void setUp() {
+        Department department = Department.builder()
+                .departmentName("IT")
+                .departmentAddress("Colomno")
+                .departmentCode("IT01")
+                .departmentId(1L)
+                .build();
+
+        Mockito.when(departmentRepository.findByDepartmentNameIgnoreCase("IT"))
+                .thenReturn(department);
+    }
+
+    @Test
+    @DisplayName("Get data based on Valid Department Name")
+    public void whenValidDepartmentName_thenDepartmentShouldFound(){
+        String departmentName = "IT";
+        Department found = departmentService.fetchDepartmentByName((departmentName));
+
+        assertEquals(departmentName, found.getDepartmentName());
+    }
+}
