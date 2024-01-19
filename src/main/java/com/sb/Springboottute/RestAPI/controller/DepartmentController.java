0aca09dfaaf9133657f7abc0aba1f29d8980c85e
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2024-01-19 11:41:18 +0530

    exception handling

--- src/main/java/com/sb/Springboottute/RestAPI/controller/DepartmentController.java
+++ src/main/java/com/sb/Springboottute/RestAPI/controller/DepartmentController.java
@@ -1,7 +1,11 @@
 package com.sb.Springboottute.RestAPI.controller;
 
 import com.sb.Springboottute.RestAPI.entity.Department;
+import com.sb.Springboottute.RestAPI.error.DepartmentNotFoundException;
 import com.sb.Springboottute.RestAPI.service.DepartmentService;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,18 +17,22 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
     @PostMapping("/departments")
-    public Department saveDepartment(@RequestBody Department department){
+    public Department saveDepartment(@Valid @RequestBody Department department){
+        LOGGER.info("Inside saveDepartment of DepartmentController");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList(){
+        LOGGER.info("Inside fetchDepartmentList of DepartmentController");
         return departmentService.fetchDepartmentList();
     }
 
     @GetMapping("/departments/{id}")
-    public Department fetchDepartmentById(@PathVariable("id") Long departmentId){
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId) throws DepartmentNotFoundException {
         return  departmentService.fetchDepartmentById(departmentId);
     }
 
